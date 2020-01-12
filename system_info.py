c99360945e2283a4f7237f7102f1418d9a0c8a39
ftshijt <728307998@qq.com>
2020-01-12 10:53:12 -0500

    system info

---
+++ system_info.py
@@ -0,0 +1,16 @@
+import sys
+import torch
+import cupy
+import cupy.cuda
+from cupy.cuda import cudnn
+
+
+def print_system_info():
+    pyver = sys.version.replace('\n', ' ')
+    print(f"python version: {pyver}")
+    print(f"pytorch version: {torch.__version__}")
+    print(f"cupy version: {cupy.__version__}")
+    print(f"cuda version: {cupy.cuda.runtime.runtimeGetVersion()}")
+    print(f"cudnn version: {cudnn.getVersion()}")
+
+print_system_info()
