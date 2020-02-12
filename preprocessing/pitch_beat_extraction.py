c84b1eaa360e876baf4cd8ded6fc2a4e9202e8d8
yiwangaosimian <306418168@qq.com>
2020-02-12 16:58:17 +0800

    Wrapped with argparse

--- preprocessing/pitch_beat_extraction.py
+++ preprocessing/pitch_beat_extraction.py
@@ -4,22 +4,28 @@
 Created on Thu Jan 30 09:34:53 2020
 
 @author: linhailan1
-只有一首歌的beats提取，pitch提取尚未完成
+
+Extract beats and save it under the same folder as the wav file
+Pitch extraction haven't finished yet!
 """
 
 
-
+import argparse
 import librosa
+import os
 
-root = '/Users/linhailan1/Desktop/林海斓/SVS/data_wave/clean/'
-name = '青梅竹马'
-path = root + name + '.txt'
-song = root + name + '.wav'
-file = open(path,'w+')
-y, sr = librosa.load(song)
-tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
-for beat in beats:
-    file.write(str(beat)+' ')
+parser = argparse.ArgumentParser()
+parser.add_argument("datadir", type=str, help="data directory")
+args = parser.parse_args()
 
-file.close()
+for root, dirs, files in os.walk(args.datadir):
+    for f in files:
+        name, suffix = f.split(".")
+        if suffix == "wav":
+            file = open((os.path.join(root, name))+'.txt', "w+")
+            y, sr = librosa.load(os.path.join(root, f))
+            tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+            for beat in beats:
+                file.write(str(beat)+' ')
+            file.close()
 
