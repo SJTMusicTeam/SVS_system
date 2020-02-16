5b24a46b5bad70b2dcc1fa030d66914efacc393f
yiwangaosimian <306418168@qq.com>
2020-02-16 16:34:36 +0800

    update beats and alignment

--- preprocessing/pitch_beat_extraction.py
+++ preprocessing/pitch_beat_extraction.py
@@ -18,14 +18,29 @@ parser = argparse.ArgumentParser()
 parser.add_argument("datadir", type=str, help="data directory")
 args = parser.parse_args()
 
+frame_length = 25/1000
+frame_shift = 10/1000
 for root, dirs, files in os.walk(args.datadir):
     for f in files:
         name, suffix = f.split(".")
         if suffix == "wav":
             file = open((os.path.join(root, name))+'.txt', "w+")
-            y, sr = librosa.load(os.path.join(root, f))
-            tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
-            for beat in beats:
+            y, sr = librosa.load(os.path.join(root, f),sr = None)
+            hop_length = int(sr * frame_shift)
+            n_fft = int(sr * frame_length)
+            tempo, beats = librosa.beat.beat_track(y=y, sr=sr, hop_length=hop_length)
+            times = librosa.frames_to_time(beats, sr=sr)
+            frames = librosa.time_to_frames(times,sr = sr, hop_length=hop_length, n_fft = n_fft)
+            #file.write(str(beats)+'\n' + str(times)+'\n' + str(frames))
+            '''
+            for i in range(len(y)):
+                if i in beats:
+                    file.write('1 ')
+                else:
+                    file.write('0 ')
+            '''
+            for beat in frames:
                 file.write(str(beat)+' ')
+            
             file.close()
 
