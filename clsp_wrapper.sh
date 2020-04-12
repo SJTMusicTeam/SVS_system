9354342c2a32e2c1c3831fc126faefef8042b507
ftshijt <ftshijt@gmail.com>
2020-04-12 02:03:14 -0400

    refactor for clsp

--- clsp_wrapper.sh
+++ clsp_wrapper.sh
@@ -1,5 +1,5 @@
 . ./path.sh
-cuda_cmd="queue-freegpu.pl --mem 4G --gpu 1 --config gpu.conf"
+cuda_cmd="queue-freegpu.pl --mem 4G --gpu 1 --config conf/gpu.conf"
 
 
 ${cuda_cmd} --gpu 1 exp/train.log train.sh
