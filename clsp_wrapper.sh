aceeb83e12db80f5cbb2afad81d798fc60f1af37
yzhan <zhangyuekai@foxmail.com>
2020-05-09 20:49:12 -0400

    add pure transformer, fix bug

--- clsp_wrapper.sh
+++ clsp_wrapper.sh
@@ -2,4 +2,4 @@
 cuda_cmd="queue-freegpu.pl --mem 8G --gpu 1 --config conf/gpu.conf"
 
 
-${cuda_cmd} --gpu 1 exp/debug_recover/train.log train_yk.sh
+${cuda_cmd} --gpu 1 exp/debug_pretrain/train.log train_yk.sh
