faa13242cd9c286162e85b19b2b72f79a71b21ed
ftshijt <728307998@qq.com>
2020-05-09 21:44:56 -0400

    test

--- clsp_wrapper.sh
+++ clsp_wrapper.sh
@@ -2,4 +2,4 @@
 cuda_cmd="queue-freegpu.pl --mem 8G --gpu 1 --config conf/gpu.conf"
 
 
-${cuda_cmd} --gpu 1 exp/debug_recover/train.log train_yk.sh
+${cuda_cmd} --gpu 1 exp/kiritan_gaussian_mask_glu/train.log train.sh
