302b576f8205492c3846f5c072ebe263b660aa9c
ftshijt <728307998@qq.com>
2021-01-21 16:11:53 -0500

    Update run.sh

--- egs/public_dataset/Natsume/run.sh
+++ egs/public_dataset/Natsume/run.sh
@@ -9,7 +9,7 @@
 stage=2
 stop_stage=2
 ngpu=0
-raw_data_dir=/Users/huonan/Downloads
+raw_data_dir=downloads
 expdir=exp/rnn
 
 # Set bash to 'debug' mode, it will exit on :
@@ -27,8 +27,9 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
   echo =======================
   echo " Stage0: download data "
   echo =======================
-#  mkdir -p ${raw_data_dir}
-  # ./local/download_from_google_drive.sh https://drive.google.com/u/0/uc\?id=1hPHwOkSe2Vnq6hXrhVtzNskJjVMQmvN_\&export=download ${raw_data_dir} zip
+  mkdir -p ${raw_data_dir}
+  echo "Due to the copyright issue,"
+  echo "Please mannually download the data from https://amanokei.hatenablog.com/entry/2020/04/30/230003"
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then 
