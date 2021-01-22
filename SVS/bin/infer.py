19b3f7e9f560463e8a4eb3422a5800db93d2603a
ftshijt <ftshijt@gmail.com>
2021-01-22 00:00:10 -0500

    Update infer.py

--- SVS/bin/infer.py
+++ SVS/bin/infer.py
@@ -2,7 +2,9 @@
 # Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 # Copyright 2020 The Johns Hopkins University (author: Jiatong Shi)
 
-"""Copyright [2020] [Jiatong Shi].
+"""
+Copyright [2020] [Jiatong Shi].
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
