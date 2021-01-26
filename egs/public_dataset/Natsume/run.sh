7c9d37230fe9f64ff108fc8bba3494cddb46d55b
ftshijt <728307998@qq.com>
2021-01-25 20:02:16 -0500

    update code

--- egs/public_dataset/Natsume/run.sh
+++ egs/public_dataset/Natsume/run.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#!/bin/bash
 
 # Copyright 2020 RUC & Johns Hopkins University (author: Shuai Guo, Jiatong Shi)
 
@@ -8,9 +8,9 @@
 
 stage=0
 stop_stage=100
-ngpu=1
+ngpu=0
 raw_data_dir=downloads
-expdir=exp/rnn_norm_perp
+expdir=exp/rnn
 
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
@@ -18,7 +18,9 @@ set -e
 set -u
 set -o pipefail
 
+
 ./utils/parse_options.sh || exit 1;
+
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then 
   # Stage0: download data
@@ -26,9 +28,8 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
   echo " Stage0: download data "
   echo =======================
   mkdir -p ${raw_data_dir}
-  echo "please download kiritan dataset from https://zunko.jp/kiridev/login.php, requires a Facebook account due to licensing issues"
-  echo "put kiritan_singing.zip under ${raw_data_dir}"
-  unzip -o ${raw_data_dir}/kiritan_singing.zip -d ${raw_data_dir}
+  echo "Due to the copyright issue,"
+  echo "Please mannually download the data from https://amanokei.hatenablog.com/entry/2020/04/30/230003"
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then 
@@ -37,8 +38,8 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   echo " Stage1: data preprocessing "
   echo ============================
 
-  python local/prepare_data.py ${raw_data_dir}/kiritan_singing/wav ${raw_data_dir}/kiritan_singing/mono_label ${raw_data_dir}/kiritan_data
-  ./local/train_dev_test_split.sh ${raw_data_dir}/kiritan_data train dev test
+  python3 local/prepare_data.py ${raw_data_dir}/Natsume_Singing_DB/wav ${raw_data_dir}/Natsume_Singing_DB/mono_label data --label_type s --wav_extention wav
+  ./local/train_dev_test_split.sh data train dev test
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then 
@@ -49,12 +50,11 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
 
   ${cuda_cmd} --gpu ${ngpu} ${expdir}/stats.log \
   train.py \
-    -c conf/train_rnn_norm_perp.yaml \
+    -c conf/train_rnn.yaml \
     --collect_stats True \
     --model_save_dir ${expdir} \
     --stats_file ${expdir}/feats_stats.npz \
     --stats_mel_file ${expdir}/feats_mel_stats.npz 
-  
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then 
@@ -63,9 +63,9 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
   echo " Stage3: train "
   echo ===============
 
-  ${cuda_cmd} --gpu ${ngpu} ${expdir}/stats.log \
+  ${cuda_cmd} --gpu ${ngpu} ${expdir}/svs_train.log \
   train.py \
-    -c conf/train_rnn_norm_perp.yaml \
+    -c conf/train_rnn.yaml \
     --model_save_dir ${expdir} \
     --stats_file ${expdir}/feats_stats.npz \
     --stats_mel_file ${expdir}/feats_mel_stats.npz
@@ -83,3 +83,4 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
 
 
 fi
+
