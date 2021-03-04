f4bcf818072b99ffba2b8906a8c32630fc92e1f8
PeterGuoRuc <2016202112@ruc.edu.cn>
2021-03-04 09:29:50 +0000

    fix oniku

--- egs/public_dataset/oniku_kurumi_utagoe_db/run.sh
+++ egs/public_dataset/oniku_kurumi_utagoe_db/run.sh
@@ -40,3 +40,46 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   --label_type ns
   ./local/train_dev_test_split.sh data train dev test
 fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then 
+  # Stage2: collect_stats
+  echo =======================
+  echo " Stage2: collect_stats "
+  echo =======================
+
+  ${cuda_cmd} --gpu ${ngpu} ${expdir}/stats.log \
+  train.py \
+    -c conf/train_rnn_norm_perp.yaml \
+    --collect_stats True \
+    --model_save_dir ${expdir} \
+    --stats_file ${expdir}/feats_stats.npz \
+    --stats_mel_file ${expdir}/feats_mel_stats.npz 
+fi
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then 
+  # Stage3: train
+  echo ===============
+  echo " Stage3: train "
+  echo ===============
+
+  ${cuda_cmd} --gpu ${ngpu} ${expdir}/svs_train.log \
+  train.py \
+    -c conf/train_rnn_norm_perp.yaml \
+    --gpu_id -1 \
+    --model_save_dir ${expdir} \
+    --stats_file ${expdir}/feats_stats.npz \
+    --stats_mel_file ${expdir}/feats_mel_stats.npz
+
+fi
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then 
+  # Stage4: inference
+  echo ===============
+  echo " Stage3: infer "
+  echo ===============
+
+  ${cuda_cmd} --gpu ${ngpu} ${expdir}/svs_infer.log \
+  infer.py -c conf/infer_rnn_norm_perp.yaml \
+  --vocoder_category wavernn
+
+fi
