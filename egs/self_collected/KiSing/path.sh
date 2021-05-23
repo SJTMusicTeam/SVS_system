a71529be402e136d2459d36cab6cbc9a528b5f97
jimmy.li <jimmy.li@zoom.us>
2021-05-23 11:42:03 +0000

    add KiSing

---
+++ egs/self_collected/KiSing/path.sh
@@ -0,0 +1,15 @@
+MAIN_ROOT=$PWD/../../..
+KALDI_ROOT=$MAIN_ROOT/tools/kaldi
+
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
+export LC_ALL=C
+
+. "${MAIN_ROOT}"/tools/activate_python.sh && . "${MAIN_ROOT}"/tools/extra_path.sh
+export PATH=$MAIN_ROOT/utils:$MAIN_ROOT/SVS/bin:$PATH
+
+export OMP_NUM_THREADS=1
+
+
+export PYTHONIOENCODING=UTF-8
